afc35aa6645ef365bcee9206de44d19be61a4464
michelynodei1 <michelynodei1@gmail.com>
2022-10-26 14:27:08 -0400

    Updated pflask
    linked my work html Updated css and copied mywork html

--- flask_app/pflask01.py
+++ flask_app/pflask01.py
@@ -11,8 +11,13 @@ app = Flask(__name__)     # create an app
 # get called. What it returns is what is shown as the web page
 @app.route('/')
 @app.route('/index')
+
 def index():
     return render_template('index.html')
+    
+@app.route('/myWork')
+def myWork():
+    return render_template('myWork.html')
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
