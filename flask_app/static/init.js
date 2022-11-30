66b27043891469902b36e4442fec9bfdaf473690
Rachel Pointer <rpointer@uncc.edu>
2022-11-30 18:05:37 -0500

    Reset repo to fix code issues

--- flask_app/static/init.js
+++ flask_app/static/init.js
@@ -38,6 +38,7 @@ function showHover() {
     }
 }
 
+
 /**
  * get search button click if short sidebar or mobile
  */
@@ -204,3 +205,27 @@ function DisplayTodos() {
 
     })
 }
+
+// Overview Javascript on tabs 
+function vopenCity(evt, cityName) {
+	var i, x, tablinks;
+	x = document.getElementsByClassName("city");
+	for (i = 0; i < x.length; i++) {
+	   x[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < x.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" w3-red", ""); 
+	}
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " w3-red";
+  }
+
+
+  /* jQuery call to the accordion() method.*/
+$(document).ready(function() {
+    $("#accordion").accordion(
+            {
+            
+            })
+});
